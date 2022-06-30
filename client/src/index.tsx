6e9b886077fb80f06341dc29c581e5ad867cf4c2
vinhdothanhpg <95896932+vinhdothanhpg@users.noreply.github.com>
2022-07-01 00:25:56 +0700

    Add tailwindcss to client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import 'index.css';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -12,6 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
       <App />
     </BrowserRouter>
   </React.StrictMode>
